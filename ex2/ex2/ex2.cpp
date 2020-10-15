f3b1424491c27f7063a0369fc41aeb6cfaba35fe
roczekmichal <mich.rocz@gmail.com>
2020-10-15 14:41:17 +0200

    'operatings_on_files'

--- ex2/ex2/ex2.cpp
+++ ex2/ex2/ex2.cpp
@@ -1,8 +1,19 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+int readInt()
+{
+    int number;
+
+    cout << "Please introduce your number: " << endl;
+    cin >> number;
+
+    return number;
+}
 
 
 int main()
@@ -10,31 +21,79 @@ int main()
     
     //Creating new file and writing in it
 
+
+    std::cout << "How many numbers you want to write ?" << std::endl;
+    int totalNumbers = readInt();
+
+    std::vector<int> values;
+
+    for (int i = 0; i < totalNumbers ; i++)
+    {
+        int temp = readInt();
+        values.push_back(temp);
+    }
+
+
     
     std::ofstream file;
-    file.open("file.txt");
+    file.open("values.txt");
 
-    file << "Writing to a file";
+    for (int i = 0; i < totalNumbers; i++)
+    {
+        file << values[i] <<"\n";
+    }
 
     file.close();
     
-
-    std::ifstream new_file("file.txt");
+    std::vector<int> read_values;
+    std::ifstream new_file("values.txt");
 
     if (new_file.is_open())
     {
         std::string line;
 
-        while(getline(new_file,line))
+        while (getline(new_file, line))
         {
-            std::cout << line << std::endl;
+            read_values.push_back(std::stoi(line));
         }
         new_file.close();
     }
     else
     {
-        std::cout << "There was a problem opening the file";
+             std::cout << "There was a problem opening the file";
     }
+
+
+    float sum = 0;
+
+    for (int i = 0; i < totalNumbers; i++)
+    {
+        sum += read_values[i];
+    }
+
+    float mean = sum / totalNumbers;
+
+    std::cout << "Mean = " << mean << std::endl;
+
+
+
+
+   // std::ifstream new_file("file.txt");
+
+   // if (new_file.is_open())
+   // {
+   //     std::string line;
+
+   //     while(getline(new_file,line))
+   //     {
+   //         std::cout << line << std::endl;
+   //    }
+   //     new_file.close();
+   // }
+   // else
+   // {
+   //     std::cout << "There was a problem opening the file";
+   // }
 
 
 
