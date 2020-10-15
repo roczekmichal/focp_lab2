c0e2bf5a5571e4e18e9fae606c2cd836e04ef95c
roczekmichal <mich.rocz@gmail.com>
2020-10-15 12:24:08 +0200

    'Check_if_your_number_is_prime'

--- ex1/ex1/ex1.cpp
+++ ex1/ex1/ex1.cpp
@@ -2,12 +2,39 @@
 
 using namespace std;
 
+
+
+
+bool isPrime(int n) 
+{
+    for (int i = 2; i < n/2; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+        
+    }
+    return true;
+}
+
+
+
 int main()
 {
+    int number;
 
-
-
-
+    cout << "Please introduce your number: " << endl;
+    cin >> number;
+    
+    if (isPrime(number))
+    {
+        cout << "Your number is prime" << endl;
+    }
+    else
+    {
+        cout << "Your number is not prime" << endl;
+    }
 
     
     return 0;
