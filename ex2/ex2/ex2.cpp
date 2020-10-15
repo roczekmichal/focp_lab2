a725eede7aa88ec1ca387a689bf352f51ee85fff
roczekmichal <mich.rocz@gmail.com>
2020-10-15 14:07:27 +0200

    'operatings_on_files'

---
+++ ex2/ex2/ex2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+    
+    //Creating new file and writing in it
+
+    
+    std::ofstream file;
+    file.open("file.txt");
+
+    file << "Writing to a file";
+
+    file.close();
+    
+
+    std::ifstream new_file("file.txt");
+
+    if (new_file.is_open())
+    {
+        std::string line;
+
+        while(getline(new_file,line))
+        {
+            std::cout << line << std::endl;
+        }
+        new_file.close();
+    }
+    else
+    {
+        std::cout << "There was a problem opening the file";
+    }
+
+
+
+
+
+    return 0;
+}
+
